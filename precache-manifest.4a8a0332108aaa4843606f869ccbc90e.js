6ca32cc0dedade22fb081a17f594b28025d78ac9
dimagong <dimagong@gmail.com>
2020-01-23 18:50:20 +0200

    Updates

---
+++ precache-manifest.4a8a0332108aaa4843606f869ccbc90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbfcee2aed68a4dd1a21b8adff87eba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4f0bcdcd7e6513ef46f",
+    "url": "/monsters-rolodex/static/css/main.a2024f31.chunk.css"
+  },
+  {
+    "revision": "4205f318e26dcb05f19d",
+    "url": "/monsters-rolodex/static/js/2.4d19465e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.4d19465e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4f0bcdcd7e6513ef46f",
+    "url": "/monsters-rolodex/static/js/main.8cfbee6d.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
